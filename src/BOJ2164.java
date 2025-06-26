31282f70e96a48425b8c069b4c0ad36681480aab
You-Hyuk <cxz6411@naver.com>
2025-06-26 23:14:17 +0900

    BOJ2164 카드2

---
+++ src/BOJ2164.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Queue;
+import java.util.LinkedList;
+import java.io.IOException;
+
+public class BOJ2164 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int count = Integer.parseInt(br.readLine());
+        Queue<Integer> queue = new LinkedList<>();
+
+        for(int i = 1; i <= count; i++) {
+            queue.add(i);
+        }
+
+        while (queue.size() > 1) {
+            queue.poll();
+            queue.add(queue.poll());
+        }
+
+        bw.write(String.valueOf(queue.poll()));
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}

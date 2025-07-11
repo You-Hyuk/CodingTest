133d623a55f09e7cabf9ddc0375c7b5a0861831e
You-Hyuk <cxz6411@naver.com>
2025-07-12 01:14:48 +0900

    BOJ25192 인사성 밝은 곰곰이

---
+++ src/BOJ25192.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+
+public class BOJ25192 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        List<String> list = new LinkedList<>();
+        Set<String> set = new HashSet<>();
+
+        int k = Integer.parseInt(br.readLine());
+        int result = 0;
+
+        for (int i = 0; i < k; i++) {
+            String str = br.readLine();
+            if (str.equals("ENTER")){
+                set.clear();
+            } else if (set.add(str)) {
+                result++;
+            }
+        }
+
+        bw.write(String.valueOf(result));
+        bw.flush();
+        bw.close();
+    }
+}

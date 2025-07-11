b6319594baedb6a26aaf4e3b36d5259b2051b3b9
You-Hyuk <cxz6411@naver.com>
2025-07-12 01:54:00 +0900

    BOJ26069 붙임성 좋은 총총이

---
+++ src/Main.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int num = Integer.parseInt(br.readLine());
+        Set<String> set = new HashSet<>();
+        set.add("ChongChong");
+
+        for (int i = 0; i < num; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String first = st.nextToken();
+            String second = st.nextToken();
+
+            if (set.contains(first) || set.contains(second)) {
+                set.add(first);
+                set.add(second);
+            }
+        }
+
+        bw.write(String.valueOf(set.size()));
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}

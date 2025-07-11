a8319558ec629d4e1af1db00d0ab1d75ed9fa3cd
You-Hyuk <cxz6411@naver.com>
2025-07-12 00:48:00 +0900

    BOJ1037 약수

---
+++ src/BOJ1037.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+import java.io.IOException;
+
+public class BOJ1037 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int num = Integer.parseInt(br.readLine());
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int min = Integer.parseInt(st.nextToken());
+        int max = min;
+        while (st.hasMoreTokens()) {
+            int input = Integer.parseInt(st.nextToken());
+            if(input < min) {
+                min = input;
+            }
+
+            if(input > max) {
+                max = input;
+            }
+        }
+
+        bw.write(String.valueOf(min * max));
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}

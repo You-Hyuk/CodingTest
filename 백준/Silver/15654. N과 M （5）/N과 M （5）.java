065dcdc6d81dc6b66966014586ebc4a9fe178c02
youhyuk_ <145669610+You-Hyuk@users.noreply.github.com>
2026-02-04 15:18:39 +0900

    [Silver III] Title: N과 M (5), Time: 252 ms, Memory: 29452 KB -BaekjoonHub

---
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.java
@@ -0,0 +1,65 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int n;
+    static int m;
+    static int[] array;
+    static int[] nums;
+    static boolean[] issued;
+    static StringBuilder stringBuilder;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+        stringBuilder = new StringBuilder();
+        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+
+        n = Integer.parseInt(stringTokenizer.nextToken());
+        m = Integer.parseInt(stringTokenizer.nextToken());
+
+        array = new int[m];
+        nums = new int[n];
+        issued = new boolean[100001];
+
+        stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+        for (int i = 0; i < n; i++) {
+            nums[i] = Integer.parseInt(stringTokenizer.nextToken());
+        }
+
+        nums = Arrays.stream(nums)
+                .sorted()
+                .toArray();
+
+        func(0);
+
+        bufferedWriter.write(stringBuilder.toString());
+        bufferedWriter.close();
+        bufferedReader.close();
+    }
+
+    static void func(int k) {
+        if (k == m) {
+            for (int i = 0; i < m; i++) {
+                stringBuilder.append(array[i]).append(" ");
+            }
+            stringBuilder.append("\n");
+            return;
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (!issued[nums[i]]) {
+                issued[nums[i]] = true;
+                array[k] = nums[i];
+                func(k + 1);
+                issued[nums[i]] = false;
+            }
+        }
+    }
+}

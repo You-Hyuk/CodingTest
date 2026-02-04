26093205ac86fa5ea24fdfdc7b6675947927c7b9
youhyuk_ <145669610+You-Hyuk@users.noreply.github.com>
2026-02-04 14:42:17 +0900

    [Silver III] Title: N과 M (3), Time: 392 ms, Memory: 67828 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+    static int n;
+    static int m;
+    static int[] array;
+    static StringBuilder stringBuilder;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+        stringBuilder = new StringBuilder();
+        String[] split = bufferedReader.readLine().split(" ");
+
+        n = Integer.parseInt(split[0]);
+        m = Integer.parseInt(split[1]);
+
+        array = new int[10];
+
+        func(0);
+
+        bufferedWriter.write(stringBuilder.toString());
+        bufferedWriter.close();
+        bufferedReader.close();
+    }
+
+    static void func(int k) {
+        if (k == m) {
+            for (int i = 0; i < m; i++) {
+                stringBuilder.append(array[i]).append(" ");
+            }
+            stringBuilder.append("\n");
+            return;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            array[k] = i;
+            func(k + 1);
+        }
+    }
+}
